069523fcd2646ef58b0c129f1fa08486ef6d9f80
hanbonghun <bng4535@khu.ac.kr>
2023-02-21 11:25:54 +0900

    work

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int num = Integer.parseInt("271");
+        int len = "271".length();
+        int answer = 0;
+            String t= "3141592";
+        for(int i=0; i<=t.length()-len; i++){
+            String s= t.substring(i,i+len);
+            if(!compare(s,"271")) answer++;
+        }
+        System.out.println(answer);
+    }
+    public static boolean compare(String a,String b){
+        for (int i = 0; i < a.length(); i++) {
+            if(a.charAt(i)-'0'> b.charAt(i)-'0'){
+                return true;
+            }else if(a.charAt(i)-'0'<b.charAt(i)-'0') return false;
+        }
+        return false;
+    }
+}
